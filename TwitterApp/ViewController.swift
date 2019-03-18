7c3dabbe50ee3261f63f403a308940c1eb12e960
varolaksoy <pixtur@icloud.com>
2019-03-18 21:14:07 +0300

    Initial Commit

---
+++ TwitterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterApp
+//
+//  Created by Mac on 18.03.2019.
+//  Copyright © 2019 Varol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
